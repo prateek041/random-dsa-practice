f30f9483a98a8b92b813cd47b07c8aa82d10c7fb
prateek041 <prateeksingh9741@gmail.com>
2022-07-02 22:18:08 +0530

    isSorted modified

--- recursion/src/com/company/IsSorted.java
+++ recursion/src/com/company/IsSorted.java
@@ -2,19 +2,27 @@ package com.company;
 
 public class IsSorted {
     public static void main(String[] args) {
-        int[] arr = {1,2,4,4,5};
-        int index = 1;
+        int[] arr = {1,2,4,4,5,6};
+        int index = 0;
         System.out.println(isSorted(arr, index));
     }
+
+//    static boolean isSorted(int[] arr, int index){
+//        if(index == arr.length-1){
+//            return true;
+//        }
+//        if(arr[index-1] > arr[index]){
+//            return false;
+//        }
+//        index++;
+//        return isSorted(arr, index);
+//    }
 
     static boolean isSorted(int[] arr, int index){
         if(index == arr.length-1){
             return true;
         }
-        if(arr[index-1] > arr[index]){
-            return false;
-        }
-        index++;
-        return isSorted(arr, index);
+
+        return arr[index] <= arr[index + 1] && isSorted(arr, index + 1);
     }
 }
